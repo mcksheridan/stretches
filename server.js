93d61a68e3a396015309fb230370930ffadccee5
Sheridan McKisick <mcksheridan@gmail.com>
2021-04-27 11:06:25 -0500

    Add Pug to project and begin creating a template for stretch routines

--- server.js
+++ server.js
@@ -4,10 +4,17 @@ import path from 'path';
 const app = express();
 const PORT = 3000;
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
 app.use(express.static(path.join(__dirname, 'src')));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'src/html/index.html'));
+});
+
+app.get('/template', (req, res) => {
+  res.render('template', { title: 'Template Page' });
 });
 
 app.listen(process.env.PORT || PORT);
